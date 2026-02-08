b56820055649f78f87bb9daf66fed42ec4ccb47b
IAFEnvoy <tanksheng545@126.com>
2026-02-08 18:52:09 +0800

    remove 1.20.4 neoforge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ stonecutter {
             .forEach { vers("$version-$it", version).buildscript = "build.$it.gradle.kts" }
 
         match("1.20.1", "forge")
-        match("1.20.4", "fabric", "neoforge")
+        match("1.20.4", "fabric")
         match("1.21.10", "fabric", "neoforge")
 
         vcsVersion = "1.21.10-neoforge"
